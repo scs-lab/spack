de88ee1b55dc25a6ad300b73769edc58ed306366
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-06 22:26:30 -0500

    fixed packages env variables

--- var/spack/repos/scs_io/packages/dlib/package.py
+++ var/spack/repos/scs_io/packages/dlib/package.py
@@ -41,18 +41,18 @@ class Dlib(CMakePackage):
         # FIXME: If not needed delete this function
         args = ['-DBUILD_SHARED_LIBS=1']
         return args
-    def set_include(self,env,path):
-        env.append_flags('CFLAGS', '-I{}'.format(path))
-        env.append_flags('CXXFLAGS', '-I{}'.format(path))
-    def set_lib(self,env,path):
-        env.prepend_path('LD_LIBRARY_PATH', path)
-        env.append_flags('LDFLAGS', '-L{}'.format(path))
-    def set_flags(self,env):
-        self.set_include(env,'{}/include'.format(self.prefix))
-        self.set_include(env,'{}/include'.format(self.prefix))
-        self.set_lib(env,'{}/lib'.format(self.prefix))
-        self.set_lib(env,'{}/lib64'.format(self.prefix))
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        self.set_flags(spack_env)
-    def setup_run_environment(self, env):
-        self.set_flags(env)
+    # def set_include(self,env,path):
+    #     env.append_flags('CFLAGS', '-I{}'.format(path))
+    #     env.append_flags('CXXFLAGS', '-I{}'.format(path))
+    # def set_lib(self,env,path):
+    #     env.prepend_path('LD_LIBRARY_PATH', path)
+    #     env.append_flags('LDFLAGS', '-L{}'.format(path))
+    # def set_flags(self,env):
+    #     self.set_include(env,'{}/include'.format(self.prefix))
+    #     self.set_include(env,'{}/include'.format(self.prefix))
+    #     self.set_lib(env,'{}/lib'.format(self.prefix))
+    #     self.set_lib(env,'{}/lib64'.format(self.prefix))
+    # def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+    #     self.set_flags(spack_env)
+    # def setup_run_environment(self, env):
+    #     self.set_flags(env)
