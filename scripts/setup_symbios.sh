c247ba2b231b7e5ccbf735b42662163375d1e425
Your Name <you@example.com>
2020-09-09 12:18:09 -0500

    fixed hcl spec

--- scripts/setup_symbios.sh
+++ scripts/setup_symbios.sh
@@ -9,8 +9,21 @@ spack install rapidjson@1.1.0%gcc@9.3.0
 spack install hiredis@1.0.0%gcc@9.3.0
 spack install openblas@0.3.10%gcc@9.3.0
 spack install hcl@master%gcc@9.3.0
+
 spack env create symbios
 spack env activate symbios
+
+spack install boost@1.74.0%gcc@9.3.0
+spack install cmake@3.17.3%gcc@9.3.0
+spack install dlib@19.17%gcc@9.3.0
+spack install mongo-cxx-driver@3.5.1%gcc@9.3.0
+spack install mpich@3.3.2%gcc@9.3.0
+spack install redis-plus-plus@1.1.2%gcc@9.3.0
+spack install rapidjson@1.1.0%gcc@9.3.0
+spack install hiredis@1.0.0%gcc@9.3.0
+spack install openblas@0.3.10%gcc@9.3.0
+spack install hcl@master%gcc@9.3.0
+
 spack load boost@1.74.0%gcc@9.3.0
 spack load cmake@3.17.3%gcc@9.3.0
 spack load dlib@19.17%gcc@9.3.0
