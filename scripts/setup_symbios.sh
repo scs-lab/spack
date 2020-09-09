f657f945affa81d8f5b419786796ae724086fa16
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-09-08 23:34:11 -0500

    fixed script

--- scripts/setup_symbios.sh
+++ scripts/setup_symbios.sh
@@ -8,13 +8,13 @@ spack install redis-plus-plus@1.1.2%gcc@9.3.0
 spack install rapidjson@1.1.0%gcc@9.3.0
 spack install hiredis@1.0.0%gcc@9.3.0
 spack install openblas@0.3.10%gcc@9.3.0
-spack install hcl@0.0.4%gcc@9.3.0
+spack install hcl@master%gcc@9.3.0
 spack env create symbios
 spack env activate symbios
 spack load boost@1.74.0%gcc@9.3.0
 spack load cmake@3.17.3%gcc@9.3.0
 spack load dlib@19.17%gcc@9.3.0
-spack load hcl@0.0.4%gcc@9.3.0
+spack load hcl@master%gcc@9.3.0
 spack load mongo-cxx-driver@3.5.1%gcc@9.3.0
 spack load mpich@3.3.2%gcc@9.3.0
 spack load redis-plus-plus@1.1.2%gcc@9.3.0
