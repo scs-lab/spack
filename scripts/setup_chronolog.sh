fa57f5f37f291a0b8e6338dabd3e04d06ca5474b
jaime <jcernudagarcia@hawk.iit.edu>
2020-09-15 13:00:21 -0500

    Changed set up scripts for chronolog and rhea

--- scripts/setup_chronolog.sh
+++ scripts/setup_chronolog.sh
@@ -8,8 +8,8 @@ spack install java@8
 spack isntall maven@3.6.3
 spack install flink@1.9.1
 
-spack env create symbios
-spack env activate symbios
+spack env create chronolog
+spack env activate chronolog
 
 spack install boost@1.74.0%gcc@9.3.0
 spack install cmake@3.17.3%gcc@9.3.0
@@ -30,6 +30,6 @@ spack load java@8
 spack load maven@3.6.3
 spack load flink@1.9.1
 
-export CXXFLAGS="-I/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/symbios/.spack-env/view/include -I/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/symbios/.spack-env/view/include/bsoncxx/v_noabi -I/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/symbios/.spack-env/view/include/mongocxx/v_noabi"
-export LDFLAGS="-L/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/symbios/.spack-env/view/lib -L/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/symbios/.spack-env/view/lib64"
-export LD_LIBRARY_PATH=/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/symbios/.spack-env/view/lib:/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/symbios/.spack-env/view/lib64
+export CXXFLAGS="-I/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/chronolog/.spack-env/view/include -I/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/chronolog/.spack-env/view/include/bsoncxx/v_noabi -I/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/chronolog/.spack-env/view/include/mongocxx/v_noabi"
+export LDFLAGS="-L/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/chronolog/.spack-env/view/lib -L/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/chronolog/.spack-env/view/lib64"
+export LD_LIBRARY_PATH=/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/chronolog/.spack-env/view/lib:/opt/ohpc/pub/software/hdevarajan/spack/v0.15.4.scs/var/spack/environments/chronolog/.spack-env/view/lib64
